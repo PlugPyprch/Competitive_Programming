89f207f52418e46d61db5bdeccaffacd8996296a
PlugPyprch <53010457+PlugPyprch@users.noreply.github.com>
2020-05-01 21:20:32 +0700

    Add files via upload

---
+++ CodeForces/703A.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+		int n,m=0,c=0;
+		cin >> n;
+		while(n--){
+			int a,b;
+			cin >> a >> b;
+			if(a>b)
+				m++;
+			else if(b>a)
+				c++;
+			else{
+				m++;
+				c++;
+			}
+		}
+		if(m>c)
+			cout << "Mishka";
+		else if(c>m)
+			cout << "Chris";
+		else
+			cout << "Friendship is magic!^^";
+}
